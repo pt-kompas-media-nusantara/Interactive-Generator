2d8e99ad3b339a32271eabc86c0ae4cebce77ff5
olightsaber <rino.dwicahyo@gmail.com>
2021-09-10 11:27:24 +0700

    update jumbotron

--- admin/js/interactive-jumbotron.js
+++ admin/js/interactive-jumbotron.js
@@ -6,17 +6,59 @@ new Vue({
     data() {
         return {
             showResult: false,
-            backgroundResult: ''
+            backgroundResult: '',
+            videoResult: '',
+            jumbotronType: 'Image',
+            subtitleResult: '',
+            titlePos: 'center',
+            titleColor: 'black',
+            videoSource: ''
         }
     },
     computed: {
         selectedBackground() {
             return this.backgroundResult;
+        },
+        titlePosResult() {
+            switch (this.titlePos) {
+                case "center":
+                    return 'justify-center items-center'
+                    break;
+                case "center-top":
+                    return 'justify-center items-start'
+                    break;
+                case "center-bottom":
+                    return 'justify-center items-end'
+                    break;
+                case "left":
+                    return 'justify-start items-center'
+                    break;
+                case "left-top":
+                    return 'justify-start items-start'
+                    break;
+                case "left-bottom":
+                    return 'justify-start items-end'
+                    break;
+                case "right":
+                    return 'justify-end items-center'
+                    break;
+                case "right-top":
+                    return 'justify-end items-start'
+                    break;
+                case "right-bottom":
+                    return 'justify-end items-end'
+                    break;
+                default:
+                    return 'justify-center items-center'
+                    break;
+            }
+        },
+        selectedVideo() {
+            return this.videoSource;
         }
     }, 
     methods: {
         galleryOpenMedia(idx) {
-            console.log('clicked')
             const self = this;
 			var file_frame;
 			file_frame = wp.media.frames.file_frame = wp.media({
@@ -33,24 +75,47 @@ new Vue({
 				document.getElementsByClassName('gallery-img-picked')[0].src = attachment.url
 				document.getElementsByClassName('inputUrl')[0].value = attachment.url
                 self.backgroundResult = attachment.url;
+
+                document.getElementsByClassName('close_media')[0].style.display = 'block'
+                document.getElementsByClassName('upload_image_button')[0].style.display = 'none'
 			});
             
 			file_frame.open();
 		}, 
+        removeMedia() {
+            const self = this;
+            self.backgroundResult = '';
+            document.getElementsByClassName('inputUrl')[0].value = '';
+            document.getElementsByClassName('gallery-img-picked')[0].src = '';
+            document.getElementsByClassName('gallery-img-picked')[0].style.display = 'none'
+
+            document.getElementsByClassName('close_media')[0].style.display = 'none'
+            document.getElementsByClassName('upload_image_button')[0].style.display = 'block'
+        },
         generateShortcode() {
             const self = this;
 
-            if (self.backgroundResult === '') {
-                alert('Silahkan isi kolom kosong')
-                return;
-            } else {
-                self.showResult = true;
-                const shortcode = `[Jumbotron  background="${self.backgroundResult}" /]`;
-
-			    self.$refs.inputResult.value = shortcode;
-                // self.previewResult = ``
-                // return `[Subtitle title="${self.subtitleResult}" background="${self.backgroundResult}" fontsize="${self.fontsizeResult}" /]`
+            let img = document.getElementsByClassName('gallery-img-picked')[0],
+                vid = document.getElementsByClassName('gallery-vid-picked')[0],
+                shortcode;
+            
+            self.showResult = true;
+            
+            // type video
+            if (img == undefined || null) {
+                this.backgroundResult = ''
+                this.videoResult = document.getElementsByClassName('gallery-vid-picked')[0].value;
+                shortcode = `{ type: '${this.jumbotronType}', url: '${this.videoResult}', title-position : '${this.titlePos}', color: '${this.titleColor}' }`;
             }
+            // type image
+            else if(vid == undefined || null) {
+                this.videoResult = ''
+                shortcode = `{ type: '${this.jumbotronType}', url: '${this.backgroundResult}', title-position : '${this.titlePos}', color: '${this.titleColor}' }`;
+            } else {
+                shortcode = `{}`
+            }
+
+            self.$refs.inputResult.value = shortcode;
 
         }
     }
