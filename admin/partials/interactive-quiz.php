b62b204042b2454bad3a5def9eac0ac76fe2ba59
Deny Ramanda <deny@kompas.com>
2021-10-25 13:08:33 +0700

    update dny

--- admin/partials/interactive-quiz.php
+++ admin/partials/interactive-quiz.php
@@ -15,21 +15,22 @@ wp_enqueue_media();
 ?>
 
 
+<div class="mt-8">
+  <p class="block text-gray-800 mb-4 font-bold">Catatan:</p>
+  <p class="block text-gray-800 mb-4">- Setiap input dengan tanda <sup class="text-red-600 font-bold text-sm">*</sup> wajib diisi. sisanya boleh dikosongkan.</p>
+</div>
 <!-- This file should primarily consist of HTML with a little bit of PHP. -->
 <section class="quizGenerator flex flex-col mt-10 w-full max-w-8xl" id="quiz">
   <!-- COVER SECTION -->
-  <div class="flex">
+  <h2 class="font-bold text-2xl leading-normal mb-2">Sampul</h2>
+  <div class="flex mb-6">
     <!-- Input Cover -->
     <section class="input_block w-full flex-1 mr-2">
       <div class="w-full">
-        <div class="mt-8">
-          <p class="block text-gray-800 mb-4 font-bold">Catatan:</p>
-          <p class="block text-gray-800 mb-4">- Setiap input dengan tanda <sup class="text-red-600 font-bold text-sm">*</sup> wajib diisi. sisanya boleh dikosongkan.</p>
-        </div>
   
         <!-- cover -->
-        <div class="md:flex md:flex-col md:items-center mb-3 p-3 rounded" 
-            style="background: #acf"  >
+        <div class="md:flex md:flex-col md:items-center mb-3 p-6 rounded" 
+            style="background: rgb(229 229 229)"  >
           <!-- Judul -->
           <div class="md:w-full flex mb-3">
             <div class="md:1/4">
@@ -72,7 +73,7 @@ wp_enqueue_media();
           <div class="md:w-full flex mb-3">
             <div class="md:1/4">
               <label class="block text-gray-800 font-bold md:text-right mb-1 md:mb-0 pr-4">
-                Thumbnail
+                Url Foto
               </label>
             </div>
             <div class="md:w-3/4 relative">
@@ -142,10 +143,12 @@ wp_enqueue_media();
   </div>
 
 
+  <h2 class="font-bold text-2xl leading-normal mb-2">Data Kuis</h2>
   <!-- KUIS BLOCK -->  
-  <div v-for="(item, key) in quiz" :key="key" class="flex mt-4" :class="updateQuizId">
+  <div v-for="(item, key) in quiz" :key="key" class="flex mb-4" :class="updateQuizId">
     <section class="input_block w-full flex-1 mr-2 question_block">
-      <div class="w-full relative flex flex-col" style="background: #acf">
+      <div class="w-full relative flex flex-col py-4 px-3" style="background: rgb(229 229 229)">
+        <span class="text-xs font-bold mb-4" style="color: #ababab;">Pertanyaan nomor {{ key+1 }}</span>
         <!-- Type -->
         <div class="w-full relative flex mt-2">
           <div class="md:w-1/4 flex justify-end">
@@ -165,7 +168,7 @@ wp_enqueue_media();
         <div class="imageMedia w-full relative mt-2 hidden">
           <div class="md:w-1/4 flex justify-end">
             <label class="block text-gray-800 font-bold md:text-right mb-1 md:mb-0 pr-4">
-              Thumbnail
+              Url Foto
             </label>
           </div>
           <div class="md:w-3/4 relative">
@@ -184,7 +187,7 @@ wp_enqueue_media();
         <div class="audioMedia w-full relative mt-2 hidden">
           <div class="md:w-1/4 flex justify-end">
             <label class="block text-gray-800 font-bold md:text-right mb-1 md:mb-0 pr-4">
-              Audio File
+              File Audio
             </label>
           </div>
           <div class="md:w-3/4 relative">
@@ -198,19 +201,19 @@ wp_enqueue_media();
         <div class="w-full relative flex">
           <div class="md:w-1/4 flex justify-end">
             <label class="block text-gray-800 font-bold md:text-right mb-1 md:mb-0 pr-4 py-4">
-              Question <sup class="text-red-600 font-bold text-sm">*</sup>
+              Pertanyaan <sup class="text-red-600 font-bold text-sm">*</sup>
             </label>
           </div>
           <div class="md:w-3/4 flex relative items-center">
-            <input class="inputQuestion md:w-11/12 bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" type="text" name="inputQuestion" placeholder="tuliskan pertanyaan disini">
+            <input class="inputQuestion md:w-11/12 bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" type="text" name="inputQuestion" placeholder="Masukkan pertanyaan disini">
           </div>
         </div>
 
         <!-- Choices -->
-        <div class="w-full relative flex">
+        <div class="w-full relative flex items-center">
           <div class="md:w-1/4 flex justify-end items-center">
             <label class="block text-gray-800 font-bold md:text-right mb-1 md:mb-0 pr-4 py-4">
-              Input Choices <sup class="text-red-600 font-bold text-sm">*</sup>
+              Jumlah Pilihan<sup class="text-red-600 font-bold text-sm">*</sup> Jawaban 
             </label>
           </div>
           <div class="md:w-3/4 flex flex-col relative justify-start">
@@ -225,7 +228,7 @@ wp_enqueue_media();
         <div class="w-full relative flex">
           <div class="md:w-1/4 flex justify-end items-center"></div>
           <div class="choiceCols md:w-3/4 flex flex-col relative justify-start">
-            <input v-for="(item, index) in choiceNumber[key]" :key="index" placeholder="choice .."
+            <input v-for="(item, index) in choiceNumber[key]" :key="index" placeholder="Text Pilihan Jawaban"
               class="inputAnswerChoice mb-2 md:w-11/12 bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" type="text">
           </div>
         </div>
@@ -234,7 +237,7 @@ wp_enqueue_media();
         <div class="w-full relative flex">
           <div class="md:w-1/4 flex justify-end">
             <label class="block text-gray-800 font-bold md:text-right mb-1 md:mb-0 pr-4 py-4">
-              Jumlah Jawaban Benar<sup class="text-red-600 font-bold text-sm">*</sup>
+              Jumlah Jawaban<sup class="text-red-600 font-bold text-sm">*</sup> Benar 
             </label>
           </div>
           <div class="md:w-3/4 flex relative items-center">
@@ -253,12 +256,12 @@ wp_enqueue_media();
               Jawaban Benar<sup class="text-red-600 font-bold text-sm">*</sup>
             </label>
           </div>
-          <div class="inputAnswerCover md:w-1/3 flex relative items-center justify-between">
-            <input class="inputAnswer mr-1" type="checkbox" id="1" name="1" value="1">A
-            <input class="inputAnswer mr-1" type="checkbox" id="2" name="2" value="2">B
-            <input class="inputAnswer mr-1" type="checkbox" id="3" name="3" value="3">C
-            <input class="inputAnswer mr-1" type="checkbox" id="4" name="4" value="4">D
-            <input class="inputAnswer mr-1" type="checkbox" id="5" name="5" value="5">E
+          <div class="inputAnswerCover md:w-1/3 flex relative items-center">
+            <input class="inputAnswer" type="checkbox" id="1" name="1" value="1"><span class="block mr-4 leading-none" style="margin-top: -6px;">A</span>
+            <input class="inputAnswer" type="checkbox" id="2" name="2" value="2"><span class="block mr-4 leading-none" style="margin-top: -6px;">B</span>
+            <input class="inputAnswer" type="checkbox" id="3" name="3" value="3"><span class="block mr-4 leading-none" style="margin-top: -6px;">C</span>
+            <input class="inputAnswer" type="checkbox" id="4" name="4" value="4"><span class="block mr-4 leading-none" style="margin-top: -6px;">D</span>
+            <input class="inputAnswer" type="checkbox" id="5" name="5" value="5"><span class="block mr-4 leading-none" style="margin-top: -6px;">E</span>
           </div>
         </div>
 
@@ -271,7 +274,7 @@ wp_enqueue_media();
         <div class="w-full relative flex mt-2 mb-2">
           <div class="md:w-1/4 flex justify-end">
             <label class="block text-gray-800 font-bold md:text-right mb-1 md:mb-0 pr-4 py-2">
-              Explanation Type <sup class="text-red-600 font-bold text-sm">*</sup>
+              Tipe Penjelasan <sup class="text-red-600 font-bold text-sm">*</sup>
             </label>
           </div>
           <select @change="updateExpType(key)"
@@ -285,7 +288,7 @@ wp_enqueue_media();
         <div class="imageExpMedia w-full relative mt-2 hidden">
           <div class="md:w-1/4 flex justify-end">
             <label class="block text-gray-800 font-bold md:text-right mb-1 md:mb-0 pr-4">
-              Thumbnail
+              Url Foto
             </label>
           </div>
           <div class="md:w-3/4 relative mb-3">
@@ -317,11 +320,11 @@ wp_enqueue_media();
         <div class="w-full relative flex mb-3">
           <div class="md:w-1/4 flex justify-end">
             <label class="block text-gray-800 font-bold md:text-right mb-1 md:mb-0 pr-4 py-4">
-              Explanation<sup class="text-red-600 font-bold text-sm">*</sup>
+              Penjelasan<sup class="text-red-600 font-bold text-sm">*</sup>
             </label>
           </div>
           <div class="md:w-3/4 flex">
-            <textarea class="inputExplanation" name="inputExp" rows="4" cols="75" placeholder="penjelasan disini" maxlength="200"></textarea>
+            <textarea class="inputExplanation p-2" name="inputExp" rows="4" cols="75" placeholder="Masukkan penjelasan disini" maxlength="200"></textarea>
           </div>
         </div>
       </div>
@@ -345,8 +348,9 @@ wp_enqueue_media();
     </section>
   
     <!-- Preview Kuis -->
+    <!-- rgb(229 229 229) -->
     <section class="preview_block w-full flex-1">
-      <div v-if="showPreview" class="w-full" style="background: #acf">
+      <div v-if="showPreview" class="w-full" style="background: rgb(229 229 229)">
         <div class="md:w-3/4 flex justify-center mx-auto">
           <p class="text-2xl font-bold rounded max-w-screen-sm text-center py-2 max-w-2xl z-10 break-words leading-tight capitalize">
             {{ updateQuizValue }}
